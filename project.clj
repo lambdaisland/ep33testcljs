e3728776332c548a29b0c119fcf2a68f81a38c3e
Arne Brasseur <arne@arnebrasseur.net>
2017-10-04 12:18:58 +0200

    Include doo as a dependency so lein cljsbuild once works
    When running `lein doo` it will automatically add doo as a dependency, but when
    running a regular `lein cljsbuild` it will fail to find the doo.* namespaces.
    This fixes that.

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject ep33testcljs "0.1.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.9.0-alpha17"]
-                 [org.clojure/clojurescript "1.9.671"]]
+                 [org.clojure/clojurescript "1.9.671"]
+                 [lein-doo "0.1.7"]]
 
   :plugins [[lein-figwheel "0.5.11"]
             [lein-cljsbuild "1.1.6"]
@@ -9,7 +10,7 @@
   :cljsbuild {:builds
               [{:id "dev"
                 :figwheel true
-                :source-paths ["src" "test"]
+                :source-paths ["src"]
                 :compiler {:main testcljs.core
                            :optimizations :none
                            :output-dir "resources/public/js/out"
